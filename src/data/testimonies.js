f5b7f7d9c4437ca412fe5dda9ed6021955ce4813
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2018-12-13 15:24:54 -0800

    added data folder

---
+++ src/data/testimonies.js
@@ -0,0 +1,59 @@
+const testimonies = [
+  {
+    text:
+      'The DeskWedge has given me the chance to do my homework more effectively and efficiently with less lower back and neck pain.The blue strips are effective for holding text books and clip boards in place. Overall the DeskWedge isn’t just an attractive tool, it has enabled me to do my homework with less pain.',
+    excerpt:
+      'The DeskWedge has given me the chance to do my homework more effectively and efficiently with less lower back and neck pain.The blue strips are effective for holding text books and clip boards in place. Overall the DeskWedge isn’t just an attractive tool, it has enabled me to do my homework with less pain.',
+    author: 'K.D.Student',
+    id: 1,
+  },
+  {
+    text:
+      'In my line of business I spend a lot of time behind the desk writing and reading. I have found the Desk Wedge to be an ideal office instrument because it greatly eases the strain on one’s back, which is caused by bending over the desk for any extended period of time. The use of the wedge prevents this effectively. I would recommend it to anyone.',
+    excerpt: '',
+    author: 'R.W. Investment Consultant',
+    id: 2,
+  },
+  {
+    text:
+      'I am a full time student at the British Columbia Institute of Technology. I was involved in a motor vehicle accident and am suffering from whiplash. As a result of my injury I am unable to sit in lectures and take notes, because of the strain put on my neck from leaning over a desk and writing. I would not be able to continue my studies if I did not have a Desk Wedge. With the Desk Wedge I am able to do homework and assignments at home. This device enables me to sit at my desk reading and writing for longer periods of time without fatigue and pain in my neck and shoulders, which are present after just a short time of working without the wedge. I do not experience any fatigue in my shoulders from writing on the wedge since it also acts as a rest for my fore arms. I am grateful to the Desk Wedge so that I can continue with my studies in my graduating term of school.',
+    excerpt: '',
+    author: 'A.K. Student',
+    id: 3,
+  },
+  {
+    text:
+      'During an accident this summer I suffered a compression fracture to two vertebrae. When I returned to school I had trouble sitting to study for even a short time. Now with the Desk Wedge I can sit for long periods of time with only slight discomfort.',
+    excerpt: '',
+    author: 'M.H. Student',
+    id: 4,
+  },
+  {
+    text:
+      'I was involved in a car accident. Not knowing about the Desk Wedge my neck was sore from studying when I had to droop my head constantly. I have been able to keep my head straight by using the Desk Wedge. Consequently my neck injury had recovered rapidly.',
+    excerpt: '',
+    author: 'G.Y. Student',
+    id: 5,
+  },
+  {
+    text:
+      'I am a fourth year, Special Education student at the University of British Columbia. As a result I spend many hours at my desk reading and writing. The strain caused by my constant slouching over my books produced painful backaches. I found the Desk Wedge to be wonderful at preventing the strain. I no longer slouch and find studying to be much less tiring. Therefore, I can work for much longer periods of time. The Desk Wedge is an ingenious device that has made studying much more comfortable.',
+    excerpt: '',
+    author: 'K.W. Student',
+    id: 6,
+  },
+  {
+    text:
+      'Your Desk Wedge pleases me by just looking at it and it so enhances my life style. I’ve shown it off so often in the hospital I’m almost afraid to nod off in case some-one swipes it. To me at present it is a much greater invention than scotch tape.',
+    excerpt: '',
+    author: 'M.P. Hospital patient',
+    id: 7,
+  },
+  {
+    text:
+      'I would like to express how happy I am with the Desk Wedge. It has helped to alleviate all the back pains I developed from working continuously at a desk. Three years go, through recommendations from my physician, I became aware of the need to correct my reading and writing posture. Before purchasing the Desk Wedge, I used several other inferior book stands. The Desk Wedge’s distinguishing features allow it to hold heavy books, support more than one book, keep the books from sliding down and allow it to be used as a writing stand.',
+    excerpt: '',
+    author: 'J.W. Office worker',
+    id: 8,
+  },
+]
